a0f08eff4654b566a3db637d31269288c8f90430
Julien <juliencaron@protonmail.com>
2021-03-10 01:40:46 +0100

    Some clean up

--- config/zsh/zshenv.zsh
+++ config/zsh/zshenv.zsh
@@ -1,12 +1,3 @@
-# ~/.profile: executed by the command interpreter for login shells.
-# This file is not read by bash(1), if ~/.bash_profile or ~/.bash_login
-# exists.
-# see /usr/share/doc/bash/examples/startup-files for examples.
-# the files are located in the bash-doc package.
-
-# the default umask is set in /etc/profile; for setting the umask
-# for ssh logins, install and configure the libpam-umask package.
-#umask 022
 
 # if running bash
 if [ -n "$BASH_VERSION" ]; then
@@ -31,6 +22,7 @@ if [ -d "$HOME/dotfiles/bin" ] ; then
     PATH="$HOME/dotfiles/bin:$PATH"
 fi
 
+# Node Version Manager (nvm)
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
